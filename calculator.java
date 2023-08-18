cd8936a84424cef3395516175b3f0fcfbf34ca79
desaianup <anup.desai@infuse.it>
2023-08-18 16:11:43 +0530

    added sub finction in java file

--- calculator.java
+++ calculator.java
@@ -2,6 +2,10 @@ public class calculator {
     static int add(int a, int b) {
         return a + b;
     }
+    static int sub(int a, int b) {
+        return a - b;
+    }
+  
 
     public static void main(String[] args) {
         int a = add(2, 3);

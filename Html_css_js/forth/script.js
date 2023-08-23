c50604cb6fa522e6e346a9972dbd287ab69ecafc
desaianup <anup.desai@infuse.it>
2023-08-23 13:32:07 +0530

    changed exercise1,3,4

--- Html_css_js/forth/script.js
+++ Html_css_js/forth/script.js
@@ -5,15 +5,11 @@ const user = {
     hobbies: ["Reading", " Listening to music", " Collecting stamps"]
 }
 
-/**
- * Use destructuting to log the following
-*/
+const { name, designation, company, hobbies } = user;
 
 const printUserProfile = () => {
     // Piyush Sharma is a Senior Software Engineer at Infuse Consulting. He likes Reading, Listening to music and Collecting stamps
-    console.log( `${user.name } is a ${user.designation } at ${user.company}. He likes ${user.hobbies}`);
-  
-    
+    console.log( `${name} is a ${designation} at ${company}.He likes ${hobbies[0]},${hobbies[1]} and${hobbies[2]}`);
 }
 
 printUserProfile()

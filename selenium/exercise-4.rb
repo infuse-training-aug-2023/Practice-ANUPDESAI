12e94f3ebdde72892c60e60183e65e8d26c64899
desaianup <anup.desai@infuse.it>
2023-08-23 18:11:03 +0530

    replace chrome with firefox to open browser

--- selenium/exercise-4.rb
+++ selenium/exercise-4.rb
@@ -2,7 +2,7 @@
 
 require 'selenium-webdriver'
 
-Selenium::WebDriver::Chrome::Service.driver_path = "C:/Users/anup.desai/Desktop/infuse/Practice-ANUPDESAI/selenium/geckodriver.exe"
+Selenium::WebDriver::Firefox::Service.driver_path = "C:/Users/anup.desai/Desktop/infuse/Practice-ANUPDESAI/selenium/geckodriver.exe"
 
 driver = Selenium::WebDriver.for :firefox
 
--- selenium/exercise-5.rb
+++ selenium/exercise-5.rb
@@ -1,6 +1,6 @@
 require 'selenium-webdriver'
 
-Selenium::WebDriver::Chrome::Service.driver_path = "C:/Users/anup.desai/Desktop/infuse/Practice-ANUPDESAI/selenium/geckodriver.exe"
+Selenium::WebDriver::Firefox::Service.driver_path = "C:/Users/anup.desai/Desktop/infuse/Practice-ANUPDESAI/selenium/geckodriver.exe"
 
 driver = Selenium::WebDriver.for :firefox
 
